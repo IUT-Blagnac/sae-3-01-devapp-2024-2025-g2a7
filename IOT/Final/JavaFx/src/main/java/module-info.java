a68c5bc3a7af7c34d52bedebdd6e73f0aa8d83f6
Romain <roro.dach@gmail.com>
2024-12-02 10:05:45 +0100

    vscode version

--- IOT/Final/JavaFx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sae_devapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sae_devapp to javafx.fxml;
-    exports com.example.sae_devapp;
-}
